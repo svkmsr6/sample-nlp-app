d858b42e76dc8aa73ee6edde961b99e4fb052a3a
Souvik Misra <souvik.misra@maersk.com>
2021-11-01 18:57:41 +0530

    Exposed new APIs

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from utils.models import CategorizerModel
 
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
-@app.get("/info/{query}")
-def info(query: str):
-    return {"detail": f"Look at this {query}"}
+model = CategorizerModel()
+
+@app.get("/model/metric")
+def get_metric():
+    return {"accuracy": f"{model.metric}"}
 
 
-@app.post("/info/add")
-async def add_info(req: Request):
+@app.post("/model/predict")
+async def predict_category(req: Request):
     data = await req.json()
-    return {"detail": f"Look at that {data['resource']}", "msg":"added resource"}
+    return {"category": model.predict_category(data['text'])}
+
+@app.post("/model/train")
+async def predict_category(req: Request):
+    data = await req.json()
+    return model.train_model(data['text'],data['label'])
 
 if __name__ == "__main__":
     # Runs the FastAPI application only if the app.py file is being run.
---
+++ utils/models.py
@@ -0,0 +1,45 @@
+import copy
+import pandas as pd
+import river
+from river.naive_bayes import MultinomialNB
+from river.feature_extraction import BagOfWords
+from river.compose import Pipeline
+
+class CategorizerModel:
+    def __init__(self):
+        self.setup()
+    
+    def setup(self):
+        self.df = pd.read_csv('./data/stock_data.csv')
+        self.df['Label'] = self.df['Sentiment'].apply(lambda s: 'bullish' if s > 0 else 'bearish')
+        self.df = self.df[['Text','Label']]
+        # Convert to Tuple
+        self.data = self.df.to_records(index=False)
+        # Build Pipeline
+        self.pipe_nb = Pipeline(('vectorizer',BagOfWords(lowercase=True)),('nb',MultinomialNB()))
+        #Initialize Metric
+        self.metric = river.metrics.Accuracy()
+        # Train
+        for text,label in self.data:
+            self.train_model(text,label)
+        print(f"Model initialized with {self.metric}")
+            
+
+    def predict_category(self,text):
+        # Make a Prediction
+        return self.pipe_nb.predict_one(text)
+
+    def metric(self):
+        return self.metric
+
+    def train_model(self,text,label):
+        init_metric = copy.deepcopy(self.metric)
+        y_pred = self.pipe_nb.predict_one(text)
+        self.pipe_nb = self.pipe_nb.learn_one(text,label)
+        self.metric = self.metric.update(label, y_pred)
+        return {
+            'metric':{
+                'new':f"{self.metric}",
+                'old': f"{init_metric}"
+            }
+        }
